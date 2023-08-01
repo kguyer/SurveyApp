ee147f37ded79646981ab77943d57fee4f532f32
kguyer <kguyer@harding.edu>
2023-08-01 13:01:59 -0500

    generate models and scaffolds

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "surveys#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :surveys do
+    get "answers", on: :member
+  end
+  resources :participants
 end
